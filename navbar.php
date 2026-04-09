6c37a4bf5b5c198a3d9d6877c9e1e90195ff7acb
Frns-W <fwhennardy@gmail.com>
2026-04-09 09:27:21 +0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-secondary-subtle" data-bs-theme="dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="http://localhost/IS63/biodata">Navbar</a>
+        <a class="navbar-brand" href="http://localhost/IS63/biodata/Index/.php">Navbar</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
